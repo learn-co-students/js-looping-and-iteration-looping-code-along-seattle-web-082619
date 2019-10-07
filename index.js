638f34cfe778ea59be01f1139c81ef61b26418a9
ianhunterharold <ianhunterharold@gmail.com>
2019-10-07 14:51:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+function birthdayThankYouLetters(array){
+  for (let i = 0; i < array.length; i++){
+    'Thank you, ${name},for the wonderful birthday gift!';
+  }
+  return array;
+}
+
+array = ["Ada","Ian","Steve"]
